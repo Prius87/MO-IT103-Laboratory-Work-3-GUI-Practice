7df4f581adc94bc8f5f1df20425059c4d147130d
Desouky <129620134+Prius87@users.noreply.github.com>
2023-04-10 01:37:58 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        MilestoneCalculator calculator = new MilestoneCalculator();
+        calculator.setVisible(true);
+    }
+}
